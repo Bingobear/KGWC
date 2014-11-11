0b2ecf88dee528709699b0d2faf6894a568d24c5
Bruns <Bruns@2803b919-26e8-4f6a-a43a-5b0cb36d15f1>
2014-11-11 15:07:49 +0000

    keygen

--- KeyGens/processing/wordcram_gen/wordcram_gen.pde
+++ KeyGens/processing/wordcram_gen/wordcram_gen.pde
@@ -5,21 +5,52 @@ something is not quite right.
 
 import wordcram.*;
 
+WordCram wordcram;
+
+void setup() {
 size(700, 400);
 background(255);
 
+
+// This code will print all the lines from the source text file.
+String[] lines = loadStrings("test.txt");
+String[] data = split(lines[0],';');
+println("there are " + lines.length +data.length+ " lines");
+println(data);
 // Each Word object has its word, and its weight.  You can use whatever
 // numbers you like for their weights, and they can be in any order.
-Word[] wordArray = new Word[] {
-    new Word("Hello", 100),
-    new Word("WordCram", 60)
-  };
+Word[] wordArray = new Word[data.length/2];
+//Word[] wordArray = new Word[data.length/2+1];
+
+int ii=0;
+int jj=0;
+while(ii<data.length-1){
+  Word word = new Word(data[ii],Integer.parseInt(data[ii+1]));
+  wordArray[jj]=word;
+  ii=ii+2;
+  jj++;
+}
+ //Word word = new Word(data[0],Integer.parseInt(data[1]));
+  //wordArray[0]=word;
+  //Word words = new Word("test2",99);
+  //wordArray[1]=words;
 
 // Pass in the sketch (the variable "this"), so WordCram can draw to it.
-WordCram wordcram = new WordCram(this)
+wordcram = new WordCram(this)
 
 // Pass in the words to draw.
-  .fromWords(wordArray);
+  .fromWords(wordArray)
+  .angledAt(0);
+}
 
+void draw() {
 // Now we've created our WordCram, we can draw it:
-wordcram.drawAll();
+  if (wordcram.hasMore()) {
+    wordcram.drawNext();
+  }
+  else {
+    println("done");
+    noLoop();
+  }
+}
+
--- KeyGens/src/main/java/PDFtoKeys/KeyGens/App.java
+++ KeyGens/src/main/java/PDFtoKeys/KeyGens/App.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 
 import com.cybozu.labs.langdetect.LangDetectException;
 
-
 /**
  * Hello world!
  * 
@@ -19,52 +18,42 @@ public class App {
 
 	}
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 		// BasicConfigurator.configure();
 
-
-
-			try {
-				parsePDFtoKey();
-			} catch (LangDetectException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-
-
-
-
-
-		System.out.println("Hello World!");
+		try {
+			parsePDFtoKey();
+		} catch (LangDetectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private static void parsePDFtoKey() throws LangDetectException, IOException {
 		PDFExtractor app = new PDFExtractor();
 		String parsedText = app.parsePdftoString();
-		
-		
+
 		LangDetect lang = new LangDetect();
 		app.setLang(lang.detect(parsedText));
 		System.out.println(app.getLang());
-		//sentence detector -> tokenizer
-		ArrayList<String> tokenheaven =app.getToken(parsedText);
+		// sentence detector -> tokenizer
+		ArrayList<String> tokenheaven = app.getToken(parsedText);
 		String[] tokenTest = new String[tokenheaven.size()];
-		for (int ii=0;ii<tokenheaven.size();ii++){
+		for (int ii = 0; ii < tokenheaven.size(); ii++) {
 			tokenTest[ii] = tokenheaven.get(ii);
 		}
 		String[] tokens = app.generalToken(parsedText);
 		ArrayList<String> keywords = app.getKeywordsfromPDF(tokens);
 		String[] filter = app.posttags(tokenTest);
-		//ArrayList<Integer> keys = app.filterNounVerb(filter);
-		ArrayList<String> keys = app.filterNoun(filter,tokens);
+		// ArrayList<Integer> keys = app.filterNounVerb(filter);
+		ArrayList<String> keys = app.filterNoun(filter, tokens);
 		ArrayList<Keyword> keyOcc = app.keyOcc(keys);
 
-
-		System.out.println("normal:" + tokens.length + ", optimiertNouns:"+keys.size()+", keywordsocc"+keyOcc.size());
+		System.out.println("normal:" + tokens.length + ", optimiertNouns:"
+				+ keys.size() + ", keywordsocc" + keyOcc.size());
 		// go go stemming
 		createTextExport(keyOcc);
 		if (keywords.isEmpty()) {
@@ -73,26 +62,31 @@ public class App {
 		} else {
 			// use extracted keywords as ref. elements
 		}
-//		app.token();
-		
+		// app.token();
+
 	}
 
 	private static void createTextExport(ArrayList<Keyword> keyOcc) {
 		Writer writer = null;
 
 		try {
-		    writer = new BufferedWriter(new OutputStreamWriter(
-		          new FileOutputStream("test.txt"), "utf-8"));
-		    for(int ii=0;ii<keyOcc.size();ii++){
-		    	Keyword current = keyOcc.get(ii);
-		    writer.write(current.word+"_"+current.occ+";");
-		    }
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream("test.txt"), "utf-8"));
+			for (int ii = 0; ii < keyOcc.size(); ii++) {
+				Keyword current = keyOcc.get(ii);
+
+				writer.write(current.word + ";" + current.occ + ";");
+
+			}
 		} catch (IOException ex) {
-		  // report
+			// report
 		} finally {
-		   try {writer.close();} catch (Exception ex) {}
+			try {
+				writer.close();
+			} catch (Exception ex) {
+			}
 		}
-		
+
 	}
 
 }
